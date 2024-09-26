0594ca90e0511e44f90afb6622c188860628e5f3
lorirothermel <lori@rothermel1.com>
2024-09-26 16:52:53 -0400

    Initial Commit

---
+++ MyTrips2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTrips2
+//
+//  Created by Lori Rothermel on 9/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTrips2/MyTrips2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyTrips2App.swift
+//  MyTrips2
+//
+//  Created by Lori Rothermel on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTrips2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
