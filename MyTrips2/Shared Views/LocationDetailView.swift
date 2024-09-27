1fb61633459cbba4ed69e3165344b14059501552
lorirothermel <lori@rothermel1.com>
2024-09-26 22:49:20 -0400

    Chapter 5 - Selecting Markers and Lookarounds

---
+++ MyTrips2/Shared Views/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  MyTrips2
+//
+//  Created by Lori Rothermel on 9/26/24.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
