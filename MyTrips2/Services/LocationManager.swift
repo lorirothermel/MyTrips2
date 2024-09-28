7740692d1d1e9d1187547dcafd2d5b619a20970c
lorirothermel <lori@rothermel1.com>
2024-09-28 09:29:48 -0400

    Chapter 7 - User Location and LocationManager

---
+++ MyTrips2/Services/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  MyTrips2
+//
+//  Created by Lori Rothermel on 9/28/24.
+//
+
+import Foundation
---
+++ MyTrips2/Shared Views/LocationDeniedView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDeniedView.swift
+//  MyTrips2
+//
+//  Created by Lori Rothermel on 9/28/24.
+//
+
+import SwiftUI
+
+struct LocationDeniedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDeniedView()
+}
