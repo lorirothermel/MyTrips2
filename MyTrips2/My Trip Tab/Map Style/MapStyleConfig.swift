bb362f17f70e157d4c9a03cbeaec429b10e2452c
lorirothermel <lori@rothermel1.com>
2024-10-02 15:33:52 -0400

    Finished App

---
+++ MyTrips2/My Trip Tab/Map Style/MapStyleConfig.swift
@@ -0,0 +1,8 @@
+//
+//  MapStyleConfig.swift
+//  MyTrips2
+//
+//  Created by Lori Rothermel on 10/2/24.
+//
+
+import Foundation
---
+++ MyTrips2/My Trip Tab/Map Style/MapStyleView.swift
@@ -0,0 +1,18 @@
+//
+//  MapStyleView.swift
+//  MyTrips2
+//
+//  Created by Lori Rothermel on 10/2/24.
+//
+
+import SwiftUI
+
+struct MapStyleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapStyleView()
+}
